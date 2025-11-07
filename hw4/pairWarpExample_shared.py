880142a4c61f5dd101dc1dafff5403b87ca0f142
PALMER SWANSON <pjswanson@fsu.edu>
2025-11-07 12:55:37 -0500

    hw4

---
+++ hw4/pairWarpExample_shared.py
@@ -0,0 +1,11 @@
+# https://fdasrsf-python.readthedocs.io/en/latest/time_warping_example.html
+	# Documentation / User Guide
+# https://github.com/jdtuck/fdasrsf_python/tree/master
+	# Github front page
+# https://github.com/jdtuck/fdasrsf_python/blob/master/notebooks/example_warping.ipynb
+	# Examples
+# https://github.com/jdtuck/fdasrsf_python/blob/master/fdasrsf/time_warping.py
+	# Used circa lines 75-160 to make pairwise warp example
+
+# Install was simple for me with
+	# pip install fdasrsf
